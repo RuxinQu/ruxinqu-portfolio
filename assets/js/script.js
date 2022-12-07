1dbfc98e7712b54abb25040923a77f853aa53751
Ruxin Qu <rosheenqu@gmail.com>
2022-12-06 21:25:06 -0800

    add favicon link

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,81 +5,87 @@ const context = {
     projects: [
         {
             title: "Wei Full",
-            img: "./img/wei-full-big.png",
+            img: "./img/project/wei-full-big.png",
             alt: "wei-full",
             page: "https://ruxinqu.github.io/Wei-Full/",
             repo: "https://github.com/RuxinQu/Wei-Full"
         },
         {
             title: "Raining Cats and Dogs",
-            img: "./img/raining-dogs-big.png",
+            img: "./img/project/raining-dogs-big.png",
             alt: "raining-cats-and-dogs",
             page: "https://raining-dogs.herokuapp.com/",
             repo: "https://github.com/RuxinQu/Raining-Cats-and-Dogs"
         },
         {
             title: "My Bolg",
-            img: "./img/my-blog.png",
+            img: "./img/project/my-blog.png",
             alt: "my-blog",
             page: "https://my-blog-4001.herokuapp.com/",
             repo: "https://github.com/RuxinQu/my-blog"
         },
         {
             title: "Weather Dashboard",
-            img: "./img/weather-dashboard-big.png",
+            img: "./img/project/weather-dashboard-big.png",
             alt: "weather-dashboard",
             page: "https://ruxinqu.github.io/weather-dashboard/",
             repo: "https://github.com/RuxinQu/weather-dashboard"
         },
         {
             title: "Note Taker",
-            img: "./img/note-taker.png",
+            img: "./img/project/note-taker.png",
             alt: "Note-Taker",
             page: "https://note-taker-4001.herokuapp.com/",
             repo: "https://github.com/RuxinQu/note-taker"
         },
         {
+            title: "Game Find Your Hat",
+            img: "./img/project/game-find-your-hat.png",
+            alt: "game-find-your-hat",
+            repo: "https://github.com/RuxinQu/game-find-your-hat"
+        },
+        {
             title: "Readme Generator",
-            img: "./img/readme-generator.png",
+            img: "./img/project/readme-generator.png",
             alt: "readme-generator",
             repo: "https://github.com/RuxinQu/readme-generator"
         },
         {
 
             title: "Team Profile Generator",
-            img: "./img/team-profile-gen.png",
+            img: "./img/project/team-profile-gen.png",
             alt: "team-profile-generator",
             repo: "https://github.com/RuxinQu/team-profile-generator"
         },
         {
             title: "E-Commerce-API",
-            img: "./img/e-commerce-api.png",
+            img: "./img/project/e-commerce-api.png",
             alt: "e-commerce-api",
             repo: "https://github.com/RuxinQu/e-commerce-api"
         },
         {
             title: "Employee Tracker",
-            img: "./img/employee-tracker.png",
+            img: "./img/project/employee-tracker.png",
             alt: "employee-tracker",
             repo: "https://github.com/RuxinQu/employee-tracker"
         },
         {
             title: "Work Day Scheduler",
-            img: "./img/scheduler.png",
+            img: "./img/project/scheduler.png",
             alt: "scheduler",
             page: "https://ruxinqu.github.io/work-day-scheduler/",
             repo: "https://github.com/RuxinQu/work-day-scheduler"
         },
         {
             title: "Code Quiz",
-            img: "./img/code-quiz-big.png",
+            img: "./img/project/code-quiz-big.png",
             alt: "code-quiz",
             page: "https://ruxinqu.github.io/code-quiz/",
             repo: "https://github.com/RuxinQu/code-quiz"
         },
         {
             title: "Password Generator",
-            img: "./img/password-generator.png",
+            img: "./img/project/password-generator.png",
             alt: "password-generator",
             page: "https://ruxinqu.github.io/password-generator/",
             repo: "https://github.com/RuxinQu/password-generator"
@@ -91,3 +97,4 @@ const compiledHtml = template(context);
 
 const displayGoals = document.getElementById('project-container');
 displayGoals.innerHTML = compiledHtml;
+
